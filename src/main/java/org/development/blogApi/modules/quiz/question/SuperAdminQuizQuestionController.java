60743dac80db0ca60b0631e37f3031cdee78b2a5
IgorKondratyuk <igorkondratyuk3@gmail.com>
2025-04-08 16:51:07 +0300

    fixed quiz sa controller route

--- src/main/java/org/development/blogApi/modules/quiz/question/SuperAdminQuizQuestionController.java
+++ src/main/java/org/development/blogApi/modules/quiz/question/SuperAdminQuizQuestionController.java
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/quiz/questions")
+@RequestMapping("api/sa/quiz/questions")
 @AllArgsConstructor
 public class SuperAdminQuizQuestionController {
 
